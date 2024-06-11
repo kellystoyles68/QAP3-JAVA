a460f7cbd18cbecd4545d55da79b5a8bef70c3b5
kellystoyles68 <150394889+kellystoyles68@users.noreply.github.com>
2024-06-11 12:38:26 -0230

    Question 3
    code and screenshoot

---
+++ Demo3.java
@@ -0,0 +1,15 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        MovablePoint movablePoint = new MovablePoint();
+        movablePoint.setXSpeed(5.0f);
+        movablePoint.setYSpeed (12.0f);
+        movablePoint.setX(5.0f);
+        movablePoint.setY (12.0f);
+        System.out.println();
+        System.out.println ();
+        System.out.println();
+        System.out.println(movablePoint);
+        System.out.println();
+        System.out.println ();
+    }
+}
